5ad172731e875850d7237b49e8050be9b46a03cd
Peter Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2023-11-16 18:42:57 +0000

    Week 3 changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject bulletPrefab;
     public float movementSpeed = 0.5f;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,7 @@ public class Player : MonoBehaviour
         
         if (Input.GetKey(KeyCode.W))
         {
-            moveDirection.y = 3;
+            moveDirection.y = 1;
         }
         else if (Input.GetKey(KeyCode.S))
         {
@@ -34,6 +35,28 @@ public class Player : MonoBehaviour
             moveDirection.x = 1;
         }
         
-        transform.Translate(moveDirection * (movementSpeed * Time.deltaTime));
+        transform.Translate(moveDirection * (movementSpeed * Time.deltaTime), Space.World);
+        
+        // transform.Rotate(new Vector3(0, 0, 100 * Time.deltaTime));
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 newDirection = mouseWorldPosition - new Vector2(transform.position.x, transform.position.y);
+        newDirection.Normalize();
+
+        float newAngle = Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, newAngle);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBullet();
+        }
+    }
+
+    void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0.0f, 0.0f, transform.eulerAngles.z));
     }
 }
